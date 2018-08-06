5cb319cb27c1482b5923cf37901b41ef448431fc
Duy <duytq@dgroup.co>
2018-08-06 20:15:31 +0700

    config webpack and react

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const CleanWebPackPlugin = require('clean-webpack-plugin')
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }, 
+  module: {
+      rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader"
+            }
+          }
+      ]
+  },
+  plugins: [
+    new CleanWebPackPlugin(['dist']),
+    new HtmlWebPackPlugin({
+    template: "./index.html",
+    filename: "./index.html"
+    })
+  ]
+};
